03f4b370e80cf483bf9b87309a5093a9b5d8c007
Hoang <cct.cry@gmai.com>
2019-12-04 16:49:50 +0700

    up file

---
+++ src/main/java/com/codegym/comfiguration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.codegym.comfiguration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
